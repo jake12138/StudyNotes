78b07bbae8cedd064223d2bae1070849e83d8273
taojie.zhang <taojie.zhang@forvia.com>
2023-07-14 17:26:31 +0800

    update

--- openGLES/code/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "WaylandWindow.h"
-
-int main(){
-    printf("hello word\n");
-    std::shared_ptr<WaylandWindow> window = WaylandWindow::getWindowSurface(400, 300);
-    return 0;
-}
